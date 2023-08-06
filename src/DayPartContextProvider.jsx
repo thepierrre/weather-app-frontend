8c5dd18ed4504d98cc9313635e365f6e9bee50c8
thepierrre <piotr.owczarczyk98@gmail.com>
2023-08-06 19:08:08 +0200

    fix the time before an API call

--- src/DayPartContextProvider.jsx
+++ src/DayPartContextProvider.jsx
@@ -1,20 +1,3 @@
-// import { useState } from "react";
-
-// import DayPartContext from "./context/day-part-context";
-
-// const DayPartContextProvider = (props) => {
-//   const { children } = props;
-//   const [isNight, setIsNight] = useState(undefined);
-
-//   return (
-//     <DayPartContext.Provider value={{ isNight, setIsNight }}>
-//       {children}
-//     </DayPartContext.Provider>
-//   );
-// };
-
-// export default DayPartContextProvider;
-
 import { useState, useEffect } from "react";
 import DayPartContext from "./context/day-part-context";
 
--- src/main.tsx
+++ src/main.tsx
@@ -23,17 +23,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </DayPartContextProvider>
   </React.StrictMode>
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-// import DayPartContextProvider from "./DayPartContextProvider.jsx";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <DayPartContextProvider>
-//       <App />
-//     </DayPartContextProvider>
-//   </React.StrictMode>
-// );
