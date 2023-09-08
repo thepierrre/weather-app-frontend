e19fff497bc5e3dfe4c6affe67fca34fd1e7afed
thepierrre <piotr.owczarczyk98@gmail.com>
2023-09-08 18:09:20 +0200

    correct the backend endpoint

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5014/",
+  // baseURL: "http://localhost:5014/",
+  baseURL: "https://get-my-current-weather-e1a1907797e1.herokuapp.com/",
 });
 
 export default instance;
